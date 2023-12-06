5598106320035ffdadc186f6e138189f301d232a
DiegoSanMo6011 <111369219+DiegoSanMo6011@users.noreply.github.com>
2023-12-05 20:24:19 -0600

    Add files via upload

---
+++ TeatroDelPueblo.cs
@@ -0,0 +1,46 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace Historic_Buildings.Buildings
+{
+    public class TeatroDelPueblo : MonumentAI
+    {
+        public override void InitializePrefab()
+        {
+            base.InitializePrefab();
+        }
+
+        public override void GetImposedDistricts(out ItemClass.Zone district0, out ItemClass.Zone district1, out ItemClass.Zone district2, out ItemClass.Zone district3)
+        {
+            // Implementa esta función según tus necesidades.
+            district0 = ItemClass.Zone.ResidentialLow;
+            district1 = ItemClass.Zone.ResidentialHigh;
+            district2 = ItemClass.Zone.CommercialLow;
+            district3 = ItemClass.Zone.CommercialHigh;
+        }
+
+        public override Color GetColor(ushort buildingID, ref Building data, InfoManager.InfoMode infoMode)
+        {
+            // Implementa esta función según tus necesidades.
+            return Color.white;
+        }
+
+        public override void GetSize(ushort buildingID, ref Building data, out int width, out int height)
+        {
+            // Implementa esta función según tus necesidades.
+            width = 4;
+            height = 4;
+        }
+
+        public override void CalculateGuestVehicles(ushort buildingID, ref Building data, ref Building.Frame frameData, ushort leaderID, ref Citizen.BehaviourData behaviour, ref int guestVehicles, ref int guestBikeVehicles)
+        {
+            // Implementa esta función según tus necesidades.
+            guestVehicles = 0;
+            guestBikeVehicles = 0;
+        }
+
+        // Aquí puedes agregar más funciones según las necesidades de tu mod.
+
+        // Recuerda ajustar las propiedades del edificio en el método Awake, OnEnable u otros, no en InitializePrefab.
+    }
+}
