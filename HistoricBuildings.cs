f5daf3b49cf90951c1496c7b28e1a9c482c11992
DiegoSanMo6011 <111369219+DiegoSanMo6011@users.noreply.github.com>
2023-12-05 20:20:34 -0600

    Add files via upload

---
+++ HistoricBuildings.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace Historic_Buildings
+{
+    public class HistoricBuildings : IUserMod
+    {
+        public string Name => "Historic Buildings";
+        public string Description => "Un mod que añade edificios históricos de México a Cities Skylines.";
+    }
+}
